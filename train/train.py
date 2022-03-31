991c1e91f132a0f5b091e1e2a8f485c7bae98364
Abhyanth P <67055477+abhyanth-learner@users.noreply.github.com>
2022-03-31 12:50:05 +0530

    Add files via upload

--- train/train.py
+++ train/train.py
@@ -68,10 +68,10 @@ def build_cnn(seq_length):
   ])
   model_path = os.path.join("./netmodels", "CNN")
   print("Built CNN.")
-#   if not os.path.exists(model_path):
-#     os.makedirs(model_path)
-#   model.load_weights("./netmodels/CNN/weights.h5")
-#   return model, model_path
+  if not os.path.exists(model_path):
+    os.makedirs(model_path)
+  model.load_weights("./netmodels/CNN/weights.h5")
+  return model, model_path
 
 
 def build_lstm(seq_length):
