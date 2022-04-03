23d5173eb8eefbe3e4fbcc68f06380ddc882c894
Abhyanth P <67055477+abhyanth-learner@users.noreply.github.com>
2022-04-03 20:55:14 +0530

    Add files via upload

--- train/train.py
+++ train/train.py
@@ -64,7 +64,7 @@ def build_cnn(seq_length):
       tf.keras.layers.Flatten(),  # (batch, 224)
       tf.keras.layers.Dense(16, activation="relu"),  # (batch, 16)
       tf.keras.layers.Dropout(0.1),  # (batch, 16)
-      tf.keras.layers.Dense(4, activation="softmax")  # (batch, 4)
+      tf.keras.layers.Dense(3, activation="softmax")  # (batch, 4)
   ])
   model_path = os.path.join("./netmodels", "CNN")
   print("Built CNN.")
