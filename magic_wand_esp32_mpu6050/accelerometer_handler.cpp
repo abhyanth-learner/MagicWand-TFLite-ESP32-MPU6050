843924123b36fd5cd31003f8cefa3894b52eb98a
Abhyanth P <67055477+abhyanth-learner@users.noreply.github.com>
2022-04-28 22:51:42 +0530

    Add files via upload

--- magic_wand_esp32_mpu6050/accelerometer_handler.cpp
+++ magic_wand_esp32_mpu6050/accelerometer_handler.cpp
@@ -16,7 +16,20 @@ limitations under the License.
 #include "accelerometer_handler.h"
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
-
+#include <Arduino.h>
+#include <Wire.h>
+#include <WireSlave.h>
+#define SDA_PIN 21
+#define SCL_PIN 22
+#define I2C_SLAVE_ADDR 0x04
+String rx_data = "";
+String x="";
+String y="";
+String z="";
+float x_final[10];
+float y_final[10];
+float z_final[10];
+void receiveEvent(int howMany);
 int begin_index = 0;
 
 Adafruit_MPU6050 mpu;
@@ -157,4 +170,73 @@ bool ReadAccelerometer(tflite::ErrorReporter* error_reporter, float* input,
     input[i] = save_data[ring_array_index];
   }
   return true;
+}
+void receiveEvent(int howMany)
+{
+    
+
+        rx_data=(char)WireSlave.read();
+//        Serial.print("Recived data:");
+//        Serial.println(rx_data);
+        if(rx_data=="x")
+        {
+          while (1 < WireSlave.available()) 
+          {
+              rx_data=(char)WireSlave.read();
+              x.concat(rx_data);
+           }
+           //Serial.println(x);
+           x_final[0]=x.toFloat();
+           x="";
+        }
+        if(rx_data=="y")
+        {
+          while (1 < WireSlave.available()) 
+          {
+              rx_data=(char)WireSlave.read();
+              y.concat(rx_data);
+           }
+           y_final[0]=y.toFloat();
+           y="";
+        }
+        if(rx_data=="z")
+        {
+          while (1 < WireSlave.available()) 
+          {
+              rx_data=(char)WireSlave.read();
+              z.concat(rx_data);
+           }
+           z_final[0]=z.toFloat();
+           z="";
+        }
+ 
+
+        
+//        Serial.println(x_final[0]);
+//        
+//        Serial.println(y_final[0]);
+//        
+//        Serial.println(z_final[0]);
+//         else{
+//         // Serial.println(data);
+//               Final_data[i]=data.toFloat();
+//          data="";
+//          i=i+1;
+//          } 
+
+     
+     //Serial.println(data);
+//     rx_data = "";
+//
+//    }
+//    for(int j=0;j<sizeof(Final_data);j++){
+//     Serial.println(Final_data[j]);  
+//    } 
+//    i=0;
+//    Serial.print("i is");
+//    Serial.println(i);
+//    int x = WireSlave.read();   
+//    Serial.println(x);  
+//    Serial.print(rx_data);
+        
 }
